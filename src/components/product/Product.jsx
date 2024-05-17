bb0d2e831f3eb5fdb39d33429a78050a7db539d7
Nahom Diro <nahomdiroj@gmail.com>
2024-05-17 15:34:55 +0300

    folders

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,7 +1,7 @@
 import React, { useState ,useEffect } from 'react'
 import axios from 'axios'
 import ProductCard from './ProductCard'
-
+import classes from './Product.module.css'
 function Product() {
 
    const [products, setProducts] = useState()
@@ -19,14 +19,13 @@ function Product() {
    
    
   return (
-    <section>
+    <section className={classes.products_container}>
        {
  
-            products.map((singleProduct)=>{
-            return   <ProductCard product={singleProduct} key={singleProduct.id}/>
-            }
-             
-            )
+        products?.map((singleProduct)=>{
+         
+          return   <ProductCard product={singleProduct} key={singleProduct.id}/>
+           })
 
          
             
--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
-
+import { Rating } from '@mui/material'
+import CurrencyFormat from '../CurrencyFormat/CurrencyFormat'
+import classes from './Product.module.css'
 function ProductCard({product}) {
+  const {image,title,id,rating,price} = product
   return (
-    <div>
+    <div className={classes.card_container}>
         <a href="">
-          <img src="{product.image}" alt="" />
+          <img src={image} alt="" />
         </a>
         <div>
-          <h3>title</h3>
-          <div>
+          <h3>{title}</h3>
+          <div className={classes.rating}>
+              <Rating value={rating.rate} precision={0.1}/>
 
+              <small>{rating.count}</small>
           </div>
           <div>
-            
+              <CurrencyFormat amount={price}/>
           </div>
+          <button className={classes.button}>
+            add to cart
+          </button>
         </div>
 
     </div>
