66e49d9e71ac512dfbe49a7572cbeb82f19076e9
Nahom Diro <nahomdiroj@gmail.com>
2024-05-30 14:28:31 +0300

    final

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance =axios.create({
-    baseURL:"http://127.0.0.1:5001/e-clone-njd/us-central1/api"
+    // baseURL:"http://127.0.0.1:5001/e-clone-njd/us-central1/api",
+
+    baseURL:"https://amazone-api-deploye.onrender.com/"
     
 })
 
